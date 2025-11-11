a7669f35975c1fa1bc1a338760d687144cc1016e
Najahal Khaira <najahal@mhs.usk.ac.id>
2025-11-11 22:01:16 +0700

    project2/function.c

--- project2/function.c
+++ project2/function.c
@@ -1,1 +1,20 @@
+#include <stdio.h>     // Library standar untuk input/output
+#include <string.h>    // Library standar untuk manipulasi string
+#include <ctype.h>     // Library standar untuk manipulasi karakter
+#include "deklarasi.h" // Include deklarasi fungction
+
+// Fungsi untuk menghapus tanda baca kecuali ' dan -, serta ubah huruf menjadi lowercase
+void rapihinKata(char *kata)
+{
+    int i = 0, j = 0;
+    while (kata[i] != '\0') // Loop sampai ketemu null terminator
+    {
+        if (isalpha(kata[i]) || kata[i] == '\'' || kata[i] == '-') // Hanya menyimpan huruf, ' dan -
+        {
+            kata[j++] = tolower(kata[i]); // Menyimpan karakter ke indeks j dan ubah ke lowercase
+        }
+        i++;
+    }
+    kata[j] = '\0'; // Akhiri string baru
+}
 
