ad8c880a394b38e4fb04bd9df1630efc2bef3476
Najahal Khaira <najahal@mhs.usk.ac.id>
2025-11-13 19:25:30 +0700

    project1/deklarasi.h

--- project1/deklarasi.h
+++ project1/deklarasi.h
@@ -19,3 +19,15 @@ typedef struct
     unsigned int jumlah; // Menyimpan jumlah alat yang dipinjam
     char tanggal[20];    // Menyimpan tanggal peminjaman alat
 } Pinjam;
+
+// ------------------- FUNGSI UTILITAS FILE -------------------
+int bacaAlat(AlatLab *list);                          // Membaca data alat dari file dan menyimpannya ke array
+void tulisAlat(AlatLab *list, int n);                 // Menulis seluruh data alat ke file
+int bacaPinjam(Pinjam *list);                         // Membaca data peminjaman dari file
+void tulisPinjam(Pinjam *list, int n);                // Menulis seluruh data peminjaman ke file
+int cekIdAlat(unsigned int id, AlatLab *list, int n); // Mengecek apakah ID alat sudah ada
+
+// ------------------- FUNGSI ADMIN -------------------
+void tambahAlat(); // Menambah data alat baru ke sistem
+void hapusAlat();  // Menghapus data alat dari sistem
+void editAlat();   // Mengedit informasi alat yang sudah ada
